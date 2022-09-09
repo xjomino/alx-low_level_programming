58f2e0d0cf6ee7888f92cb9653130c316d01a28c
xjomino <jammwa@gmail.com>
2022-09-09 11:21:51 +0000

    new file:   4-print_alphabt.c 	new file:   5-print_numbers.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * printing random numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char b = 'a';
+
+	while (b <= 'z')
+	{
+		if (b != 'e' && b != 'q')
+		{
+			putchar(b);
+		}
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * printing random numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b <= 9; b++)
+		printf("%d", b);
+	printf("\n");
+	return (0);
+}
