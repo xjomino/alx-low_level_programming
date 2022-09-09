55b0cd9bbada5da712bb3c19f4978a10e42fe155
xjomino <jammwa@gmail.com>
2022-09-09 12:26:38 +0000

    chop chop

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * printing random numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 48; b <= 57; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
