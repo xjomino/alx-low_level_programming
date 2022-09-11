28f339e35ea368645df94be80ac5c838ee8ac3ba
xjomino <jammwa@gmail.com>
2022-09-11 02:42:37 +0000

    anotherputs way

---
+++ 0x00-hello_world/otherputs1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char MYSTR[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(MYSTR);
+	return (0);
+}
