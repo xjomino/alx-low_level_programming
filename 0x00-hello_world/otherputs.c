bfb012d8e72a6221789a6196a11f267106be25b6
xjomino <jammwa@gmail.com>
2022-09-11 02:27:58 +0000

    another way of using function put

---
+++ 0x00-hello_world/otherputs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char string[70];
+
+	strcpy(string, "\"Programming is like building a multilingual puzzle");
+	puts(string);
+	return (0);
+}
