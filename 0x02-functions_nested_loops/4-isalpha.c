084bf22a5c2761d3ea5a414418bce295d2b82508
xjomino <jammwa@gmail.com>
2022-09-14 10:24:14 +0000

    alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha -checks for alphabets
+ * @c: the character being checked
+ *
+ * Return: 1 if c is letter, if not 0
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c = 'A' && c <= 'Z'));
+}
