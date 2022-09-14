81c877e13e8a827a74b3049a4f0909139a899101
xjomino <jammwa@gmail.com>
2022-09-14 08:55:06 +0000

    prototype

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,10 @@
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ *main - header file for protoypes
+ *void protoypes (void)
+ *int protoypes (int)
+ */
+int _putchar(char);
+
+#endif /* MAIN_H */
