1935c494f0fc720307b5688a0edde2dc653f7c37
xjomino <jammwa@gmail.com>
2022-09-15 16:14:35 +0000

    more nested functions

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ *main - header file for protoypes
+ *void protoypes (void)
+ *int protoypes (int)
+ */
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+#endif /* MAIN_H */
