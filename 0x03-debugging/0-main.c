bef639c199f06ac8ae28765a6200b6482900bf68
xjomino <jammwa@gmail.com>
2022-09-16 02:17:32 +0000

    corrections

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
-  * main - tests if integer is positive or negative
+  * main - tests if integer is 
+  * positive or negative
   * Return: 0
   */
 int main(void)
 {
-int i;
+	int i;
 
-i = 0;
+	i = 0;
 
-positive_or_negative(i);
-return (0);
+	positive_or_negative(i);
+	return (0);
 }
