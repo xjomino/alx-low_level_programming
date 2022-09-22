d8e05aa842ababd5031d6129b35cb9b5fbc93283
xjomino <jammwa@gmail.com>
2022-09-22 11:52:59 +0000

    corrections

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,8 @@ int _strcmp(char *s1, char *s2)
 
 	for (k = 0; s1[k] != '\0' || s2[k] != '\0'; k++)
 	{
-		return (s1[k] - s2[k]);
+		if (s1[k] != s2[k])
+			return (s1[k] - s2[k]);
 	}
 	return (0);
 }
