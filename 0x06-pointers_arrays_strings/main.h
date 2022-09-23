c3a7eeee5b95fff578f0fe400b180b75f8f72b96
xjomino <jammwa@gmail.com>
2022-09-23 01:49:37 +0000

    modified:   6-cap_string.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H
-#define MAIN_H
-/**
- * main - header file for protoypes
- * void protoypes (void)
- * int protoypes (int)
- */
+#define MAIN_
+
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -17,4 +14,5 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
+char *leet(char *);
 #endif /* MAIN_H */
---
+++ 0x06-pointers_arrays_strings/mine
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reverse_array - reverse array of int
+ * @a: the array
+ * @n: upto the nth to be reversed)
+ */
+void reverse_array(int *a, int n)
+{
+	int k, b;
+
+	for (k = 0; k < n / 2; k++)
+	{
+		b = a[k];
+		a[k] = a[n - k - 1];
+		a[n - k - 1] = b;
+	}
+}
