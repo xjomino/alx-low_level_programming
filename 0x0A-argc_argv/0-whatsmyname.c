ab498ee3fa8427210125fb0a0befb52b08414dbe
xjomino <jammwa@gmail.com>
2022-09-30 11:37:25 +0000

    corrections

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 /**
  * main - print the name of the progarm
  * @argc: number of arguments
@@ -8,6 +8,6 @@
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }
