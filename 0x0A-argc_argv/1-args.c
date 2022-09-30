e2349997e0585f58a859a2247d51e32e68203b61
xjomino <jammwa@gmail.com>
2022-09-30 11:07:32 +0000

    arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print the name of the progarm
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef FILE_MAIN_H
+#define FILE_MAIN_H
+
+int _putchar(char c);
+int _atoi(char *s);
+#endif
