e29f985eddce595b3ba2bda93f1888c4a14df808
xjomino <jammwa@gmail.com>
2022-10-07 10:06:02 +0000

    corrections to task

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,4 +7,5 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
 #endif
