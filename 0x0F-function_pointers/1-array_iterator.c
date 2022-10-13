df7bc9d489ba7314a167f8d876da2b245a7e83fb
xjomino <jammwa@gmail.com>
2022-10-13 11:11:13 +0000

    test

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - that executes a function given
+ * as a parameter on each element of an array
+ * @array: array of ints
+ * @size: size of array
+ * @action: pointer to the action function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t k;
+
+	for (k = 0; k < size; k++)
+		action(array[k]);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+/**
+ * int_index - function that searches for an integer
+ * @array: array of ints
+ * @size: size of array
+ * @cmp: pointer function
+ * Return: the index of the first element for
+ * which the cmp function does not return 0
+ * If no element matches, return -1
+ * If size <= 0, return -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int k;
+
+	if (size < 1 || array == NULL || cmp == NULL)
+		return (-1);
+	for (k = 0; k < size; k++)
+	{
+		if (cmp(array[k]))
+			return (k);
+	}
+	return (k);
+}
