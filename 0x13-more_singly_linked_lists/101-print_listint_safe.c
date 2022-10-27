7a00b3c5ed3004beacf3004dc6939f64d4000246
xjomino <jammwa@gmail.com>
2022-10-27 12:59:33 +0000

    corrections

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,12 +1,13 @@
 #include "lists.h"
 #include <stdio.h>
-
 /**
- *  * print_listint_safe - Print a `listint_t` linked list including mem addresses
- *   * @head: head of linked list
- *    * Description: Go through the list only once.
- *     * Return: number of nodes in list. If fails, exit with status 98.
- *      */
+ * print_listint_safe - Print a `listint_t` linked
+ * list including mem addresses
+ * @head: head of linked list
+ * Description: Go through the list only once.
+ * Return: number of nodes in list. If fails,
+ * exit with status 98.
+ */
 size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *tmp;
