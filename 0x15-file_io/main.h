0c0d9239454c44261dc2cefcce35c51020d2ae37
xjomino <jammwa@gmail.com>
2022-11-03 02:44:38 +0000

    test

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,13 @@
 #ifndef FILE_MAIN_H
 #define FILE_MAIN_H
 
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#define MAXSIZE 1204
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
